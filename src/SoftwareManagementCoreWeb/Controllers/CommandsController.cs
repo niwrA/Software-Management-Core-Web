df6a82db8b165c001106a45f50f0d16263a97291
niwrA <arwin@niwra.nl>
2017-01-17 23:10:32 +0100

    Hard links to local debug output (will be replaced with Nuget packages pushed from CI), for some initial experimentation with commands coming in from http post

---
+++ src/SoftwareManagementCoreWeb/Controllers/CommandsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CommandsShared;
+using ProductsShared;
+using SoftwareManagementEFCoreRepository;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SoftwareManagementCoreWeb.Controllers
+{
+    [Route("api/[controller]")]
+    public class CommandsController : Controller
+    {
+        // GET: api/commands
+        [HttpGet]
+        public IEnumerable<ICommand> Get()
+        {
+            return new List<ICommand> { new CreateProductCommand(), new RenameProductCommand() };
+        }
+
+        // POST api/commands
+        [HttpPost]
+        public void Post([FromBody]IEnumerable<ICommand> commands)
+        {
+            // simple, direct execution like transaction
+            // todo: save commands, then execute, for eventsourcing
+            // todo: decide if mapped by $type or if manually mapped by CommandTypeId (probably,
+            // combined with eventsourcing)
+
+            // consider as transaction
+            var repository = new MainRepository(new MainContext());
+            foreach (var command in commands)
+            {
+                // get repository by command.CommandTypeId 
+                command.EntityRepository = repository;
+                command.Execute();
+            }
+
+            // complete transaction
+            repository.PersistChanges();
+        }
+
+    }
+}
